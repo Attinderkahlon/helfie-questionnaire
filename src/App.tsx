3566e71dfdb0f64382f5be6714f4d4fc88a4157a
Attinder <Attinderkahlon@gmail.com>
2022-12-05 21:14:31 +1100

    added new images and landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import LandingPage from './components/layout/LandingPage'
 import Navbar from './components/layout/Navbar'
 import Questionnaire from './pages/questionnaire'
 
@@ -9,15 +10,19 @@ export default function App() {
   return (
     <div className='max-w-md mx-auto min-h-screen grid'>
       <Navbar />
+      {!openBottomSheet && <LandingPage />}
+
       <div
-        className={`bg-gray-50 transition-all duration-300 mt-auto  ${
+        className={`bg-gray-50 transition-all duration-500 mt-auto  ${
           openBottomSheet
             ? 'h-full'
             : 'h-16 flex rounded-t-xl items-center justify-center'
         }`}
         onClick={() => setOpenBottomSheet(true)}
       >
-        {!openBottomSheet && <span className='text-blue-600'>Check Now</span>}
+        {!openBottomSheet && (
+          <span className='text-blue-600 font-bold text-xl'>Check Now</span>
+        )}
         {openBottomSheet && <Questionnaire />}
       </div>
     </div>
